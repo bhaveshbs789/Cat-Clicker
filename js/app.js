102d00a7fb45076fc409aa01ce07411a3af306d2
Bhavesh Sharma <bhaveshbs789@gmail.com>
2018-11-03 15:36:19 +0530

    Updated the Eventlistener for adding number of clicks

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-var clickValue = document.getElementById("click-count");
-var count = clickValue.innerHTML;
+// var clickValue = document.getElementById("click-count");
+// var count = clickValue.innerHTML;
 //var catPic = document.getElementById("cat-pic-elem"); // for Requirement 1
 
 var imageDiv = document.getElementById("image");
@@ -18,12 +18,12 @@ var imageDiv = document.getElementById("image");
 /*
 -----Below for requirement 2
 */
-imageDiv.addEventListener("click", function(e){
-	if(e.target.nodeName == "IMG"){
-		count = parseInt(count)+1;
-		clickValue.innerHTML = count;
-	}
-})
+// imageDiv.addEventListener("click", function(e){
+// 	if(e.target.nodeName == "IMG"){
+// 		count = parseInt(count)+1;
+// 		clickValue.innerHTML = count;
+// 	}
+// })
 
 
 // document.addEventListener('change', function(){
@@ -31,13 +31,38 @@ imageDiv.addEventListener("click", function(e){
 	
 // })
 
-document.addEventListener('change', function(){
-	const allInputs = document.querySelectorAll('input');
-	allInputs.forEach(function(elem){
-		if(elem.checked){
-			let catPic = document.querySelector('#'+elem.value + '-img');
-			catPic.classList.toggle('hide');
-		}
-		//catPic.classList.toggle()
-	})
-})
+// document.addEventListener('change', function(){
+// 	const allInputs = document.querySelectorAll('input');
+// 	allInputs.forEach(function(elem){
+// 		if(elem.checked){
+// 			let catPic = document.querySelector('#'+elem.value + '-img');
+// 			catPic.classList.toggle('hide');
+// 		}
+// 		//catPic.classList.toggle()
+// 	})
+// })
+
+// const z = document.querySelectorAll('input');
+
+// z.forEach(function(element,index){
+// 	element.addEventListener('change',function(){
+// 		if(element.checked){
+// 			let imgDiv = document.getElementById(element.value + '-img');
+// 			imgDiv.classList.remove('hide')
+// 		} else {
+// 			imgDiv.classList.add('hide');
+// 		}
+// 	})
+// 	console.log(element.checked);
+// })
+
+imageDiv.addEventListener('click', function(e){
+	if(e.target.nodeName == 'IMG'){
+		let b = e.target.parentElement.previousElementSibling.firstElementChild;
+		console.log(b);
+		let catClickCount = b.innerText;
+		b.innerText = parseInt(catClickCount) + 1;
+	}
+});
+
+
